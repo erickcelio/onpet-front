d3412eee3a6496e66311b1ba8839fff483eb546b
Erick Celio <erickcelio@hotmail.com>
2021-05-05 16:02:35 -0300

    feat: creating steps and some styles

--- containers/Register/index.js
+++ containers/Register/index.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+import styles from "./register.module.scss";
+
 function RegisterContainer() {
   const [step, setStep] = useState(1);
 
@@ -7,6 +9,11 @@ function RegisterContainer() {
     <div className="w-full h-full">
       <div className="h-[200px] w-full bg-blue-700 text-white">
         <h1 className="text-5xl">Bem Vindo!</h1>
+        <ul className={styles.stepsContainer}>
+          <li className={styles.step}>1</li>
+          <li className={styles.step}>2</li>
+          <li className={styles.step}>3</li>
+        </ul>
       </div>
     </div>
   );
